aa5be661ee3ecf10b827d5021c9f0c63d5d94b31
Lucas Santos <lucasantosdevweb@gmail.com>
2024-12-31 13:07:19 -0300

    sbyte type lesson

--- programming-c-sharp-base/Program.cs
+++ programming-c-sharp-base/Program.cs
@@ -2,6 +2,16 @@
   {
     static void Main(string[] args)
     {
-      System.Console.WriteLine("Hello, world!");
+      /*
+      - O tipo sbyte tem a capacidade de armazenar valores de até 1 Byte.
+      - Esses valores vão de -128 até 127.
+      - Ele pode ser sinalizado visto que o número pode ser negativo.
+      */
+      sbyte number = 127;
+      /*
+      - Caso 
+      */
+      // sbyte number = 128; || Gera um erro de compilação pois o valor ecxedeu os valores limites deste tipo
+      System.Console.WriteLine(number);
     }
   }
--- programming-c-sharp-base/obj/Debug/net9.0/programming-c-sharp-base.AssemblyInfo.cs
+++ programming-c-sharp-base/obj/Debug/net9.0/programming-c-sharp-base.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("programming-c-sharp-base")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c057d3b715f8f71e7c31444d50953fe947d1b15")]
 [assembly: System.Reflection.AssemblyProductAttribute("programming-c-sharp-base")]
 [assembly: System.Reflection.AssemblyTitleAttribute("programming-c-sharp-base")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
